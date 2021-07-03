c84813514c87ef558ef5b3dcbf69915e41fcd4c4
Rodrigo Bilbao <bilbaorodrigo6@gmail.com>
2021-07-03 10:01:40 -0400

    ready

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React,{ Fragment } from "react"
+
+
+export const About = () => (
+    <Fragment>
+        <h1>About</h1>
+        <p>Ayudo a su marca a navegar a la conversacion social,acercarse a influenciadores y generar coberturas en los medios. Desde el desarrollo de sitios web y apps, coloco a su empresa en el centro de la conversacion digital.</p>
+        <p>Ayudo a su marca a navegar a la conversacion social,acercarse a influenciadores y generar coberturas en los medios. Desde el desarrollo de sitios web y apps, coloco a su empresa en el centro de la conversacion digital.</p>
+    </Fragment>
+)
